5744b873409fea7890d7260b08b828c220917b90
maxnazaruk <max.nazaruk@gmail.com>
2020-07-30 22:09:36 +0300

    second commit

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,19 +1,46 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.css.Style;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    Scene scene1;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Button btn = new Button();
+        //Border border = new Border();
+        btn.setMaxWidth(25);
+        btn.setMaxHeight(25);
+        btn.setTranslateX(0);
+        btn.setTranslateY(-300);
+        StackPane layout1 = new StackPane();
+        BorderPane root1 = new BorderPane();
+        root1.setCenter(btn);
+        //root1.setCenter();
+        root1.setStyle("-fx-background-color: #0A184F;");
+        scene1 = new Scene(root1, 800, 800);
+        primaryStage.setScene(scene1);
         primaryStage.show();
+
+        scene1.setOnKeyPressed(e -> {
+            if (e.getCode() == KeyCode.A) {
+                btn.setTranslateX(0);
+                btn.setTranslateY(btn.getTranslateY() + 25);
+            }
+        });
+
     }
 
 
